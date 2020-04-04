ef92516080942130585cdabc3ef85c2b11881a4c
Rachel Padworski <rachelpadworski@gmail.com>
2020-04-04 23:30:59 +0000

    Done.

---
+++ lib/class_methods_module.rb
@@ -0,0 +1,9 @@
+require_relative './class_methods_module.rb'
+require_relative './dance_module.rb'
+
+module MetaDancing
+  
+  def metadata
+    "This class produces objects that love to dance."
+  end
+end
